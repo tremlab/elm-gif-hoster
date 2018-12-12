2335b0a7bb6f1d3e46ed4623e8665be68e4982d6
tremlab <rbalmert@gmail.com>
2018-12-12 15:11:13 -0800

    more gif fun

--- src/ReneeGifLibrary.elm
+++ src/ReneeGifLibrary.elm
@@ -30,4 +30,34 @@ library =
 
       -- ip = "Minions"
       }
+    , { url = "http://drinkwhen.ca/wp-content/uploads/2017/01/kiss-kiss-bang-bang-swear.gif"
+      , keywords = [ "girl", "swear" ]
+
+      -- ip = "Kiss Kiss Bang Bang"
+      }
+    , { url = "https://us.v-cdn.net/6029252/uploads/editor/qu/3h8jg3msvcie.gif"
+      , keywords = [ "girl", "power" ]
+
+      -- ip = "Avengers"
+      }
+    , { url = "https://media.giphy.com/media/fBNQRhWth737a/giphy.gif"
+      , keywords = [ "laughing" ]
+
+      -- ip = "Cowboy Bebop"
+      }
+    , { url = "https://media.giphy.com/media/5iw1uEuhN0PnO/giphy.gif"
+      , keywords = [ "coding" ]
+
+      -- ip = "Cowboy Bebop"
+      }
+    , { url = "https://media.giphy.com/media/9n26IAEIA6yc0/giphy.gif"
+      , keywords = [ "groovy", "stylin", "duo", "dance" ]
+
+      -- ip = "Down with Love"
+      }
+    , { url = "https://68.media.tumblr.com/b8bae53d493234e828a6dec1fe6a79a9/tumblr_oi03pxzoZM1s2wio8o1_1280.gif"
+      , keywords = [ "girl", "what" ]
+
+      -- ip = "Star Wars"
+      }
     ]
